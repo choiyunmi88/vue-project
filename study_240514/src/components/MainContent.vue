a72e9fdd97364a8336d1bb624344f3104140d65b
choiyunmi-ui <79549646+choiyunmi-ui@users.noreply.github.com>
2024-05-14 19:18:06 +0900

    first commit

---
+++ study_240514/src/components/MainContent.vue
@@ -0,0 +1,170 @@
+<script setup lang="ts">
+import '@/assets/css/main.css'
+</script>
+<template>
+  <!--  (s) main -->
+  <div class="main">
+    <div class="header">
+      <div class="logo">
+        <div class="logo-svg">
+          <svg
+            class="vector-container"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M7.19296 0C5.6621 0 4.42108 1.24208 4.42108 2.77427V21.2257C4.42108 22.7579 5.6621 24 7.19296 24C8.72383 24 9.96485 22.7579 9.96485 21.2257V5.54854H16.7236C18.2544 5.54854 19.4955 4.30646 19.4955 2.77427C19.4955 1.24208 18.2544 0 16.7236 0H7.19296ZM14.2393 15.5702C15.7729 15.5702 17.0161 14.327 17.0161 12.7934C17.0161 11.2598 15.7729 10.0165 14.2393 10.0165C12.7056 10.0165 11.4624 11.2598 11.4624 12.7934C11.4624 14.327 12.7056 15.5702 14.2393 15.5702Z"
+              fill="#F5F5F5"
+            />
+          </svg>
+        </div>
+      </div>
+      <div class="button">
+        <div class="label">Register Now</div>
+      </div>
+    </div>
+    <div class="hero">
+      <div class="content">
+        <div class="title">Faster Landing pages, <br />Better Templates</div>
+        <div class="subtitle">
+          Use one library with any framework you have. Creative and Beautiful
+          <br />websites now made easy with One Ui Design Templates.
+        </div>
+        <div class="button2">
+          <div class="label">Get Started Now</div>
+        </div>
+      </div>
+      <img class="placeholder" src="../assets/images/placeholder.jpg" />
+    </div>
+    <div class="feature">
+      <div class="content">
+        <div class="title2">Services We Offer</div>
+        <div class="subtitle">
+          Use one library with any framework you have. Creative and Beautiful
+          <br />websites now made easy with One Ui Design Templates.
+        </div>
+      </div>
+      <div class="boxes">
+        <div class="content-box">
+          <div class="icon">
+            <div class="square-plus">circle-nodes</div>
+          </div>
+          <div class="contents">
+            <div class="title3">Web Design</div>
+            <div class="body">
+              a book or other written or printed work, regarded in terms of its content rather than
+              its physical form.
+            </div>
+            <div class="button3">
+              <div class="label2">Read More</div>
+              <div class="chevron-right">chevron-right</div>
+            </div>
+          </div>
+        </div>
+        <div class="content-box">
+          <div class="icon">
+            <div class="square-plus">bars-staggered</div>
+          </div>
+          <div class="contents">
+            <div class="title3">Content Writing</div>
+            <div class="body">
+              a book or other written or printed work, regarded in terms of its content rather than
+              its physical form.
+            </div>
+            <div class="button3">
+              <div class="label2">Read More</div>
+              <div class="chevron-right">chevron-right</div>
+            </div>
+          </div>
+        </div>
+        <div class="content-box">
+          <div class="icon">
+            <div class="square-plus">location-dot</div>
+          </div>
+          <div class="contents">
+            <div class="title3">SEO</div>
+            <div class="body">
+              a book or other written or printed work, regarded in terms of its content rather than
+              its physical form.
+            </div>
+            <div class="button3">
+              <div class="label2">Read More</div>
+              <div class="chevron-right">chevron-right</div>
+            </div>
+          </div>
+        </div>
+        <div class="content-box">
+          <div class="icon">
+            <div class="square-plus">layer-group</div>
+          </div>
+          <div class="contents">
+            <div class="title3">Hosting</div>
+            <div class="body">
+              a book or other written or printed work, regarded in terms of its content rather than
+              its physical form.
+            </div>
+            <div class="button3">
+              <div class="label2">Read More</div>
+              <div class="chevron-right">chevron-right</div>
+            </div>
+          </div>
+        </div>
+        <div class="content-box">
+          <div class="icon">
+            <div class="dribbble"></div>
+          </div>
+          <div class="contents">
+            <div class="title3">Ui Design</div>
+            <div class="body">
+              a book or other written or printed work, regarded in terms of its content rather than
+              its physical form.
+            </div>
+            <div class="button3">
+              <div class="label2">Read More</div>
+              <div class="chevron-right">chevron-right</div>
+            </div>
+          </div>
+        </div>
+        <div class="content-box">
+          <div class="icon">
+            <div class="square-plus">object-ungroup</div>
+          </div>
+          <div class="contents">
+            <div class="title3">Content Box</div>
+            <div class="body">
+              a book or other written or printed work, regarded in terms of its content rather than
+              its physical form.
+            </div>
+            <div class="button3">
+              <div class="label2">Read More</div>
+              <div class="chevron-right">chevron-right</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cta">
+      <div class="content">
+        <div class="title4">Expert Opinion 1 Click Away</div>
+        <div class="subtitle">
+          Use one library with any framework you have. Creative and Beautiful
+          <br />websites now made easy with One Ui Design Templates.
+        </div>
+      </div>
+      <div class="button2">
+        <div class="label3">Contact Us</div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="footer-bottom">
+        <div class="copyrights">@ 2023 One Agency. All rights reserved.</div>
+      </div>
+    </div>
+  </div>
+  <!--  (e) main -->
+</template>
